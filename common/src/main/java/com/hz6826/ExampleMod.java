692ba5e16ff61d4c69f1fad91eeeb451db355886
Oak Platinum <oakplatinum@qq.com>
2024-12-15 17:27:10 +0800

    Initial commit

---
+++ common/src/main/java/com/hz6826/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.hz6826;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "structure_saplings";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric-like/src/main/java/com/hz6826/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package com.hz6826.fabriclike;
+
+import com.hz6826.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
